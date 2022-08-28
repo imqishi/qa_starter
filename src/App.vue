d943ef9d6ad42d2ee090e97ba62e3ae503f69235
nickqi <nickqi@tencent.com>
2022-08-28 11:15:56 +0800

    支持选择题目类型

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,61 @@
   <div id="app">
     <router-view></router-view>
     <group>
-      <cell title="单选题" :value="'每个题目仅一个选项正确，每题'+selectScore+'分，共'+selectScore*form.selectAns.length+'分'"></cell>
-    </group>
-    <div v-for="(ans, idx) in form.selectAns" :key="'sa' + idx">
-      <checklist :title="'问题'+ (idx + 1) +'：' + selectData[idx].Problem" :label-position="'right'" required :max="1"
-        :min="1" :options="selectData[idx].Opts" v-model="ans.value"></checklist>
-        <!--
-      <group>
-        <cell v-show="hasSubmit"
-          :title="singleAnsRight(ans.value, selectData[idx].Ans[0]) ? '恭喜您答对了':'回答错误，答案应为'+selectData[idx].Ans[0]"
-          :style="{color: singleAnsRight(ans.value, selectData[idx].Ans[0])? 'green':'red'}">
-        </cell>
-      </group>
-        -->
-    </div>
-    <group>
-      <cell title="多选题" :value="'每个题目有一个或多个选项正确，每题'+multiSelectScore+'分，共'+multiSelectScore*form.multiSelectAns.length+'分'"></cell>
-    </group>
-    <div v-for="(ans, idx) in form.multiSelectAns" :key="'msa' + idx">
-      <checklist :title="'问题'+ (idx + 1) +'：' + multiSelectData[idx].Problem" :label-position="'right'" required
-        :max="multiSelectData[idx].Opts.length" :min="1" :options="multiSelectData[idx].Opts" v-model="ans.value">
+      <checklist title="出题类型" :label-position="'right'" required
+        :max="showProblemOpts.length" :min="1" :options="showProblemOpts" v-model="showProblemSetting">
       </checklist>
-        <!--
-      <group>
-        <cell v-show="hasSubmit"
-          :title="multiAnsRight(ans.value, multiSelectData[idx].Ans) ? '恭喜您答对了':'回答错误，答案应为'+getMultiAnsStr(multiSelectData[idx].Ans)"
-          :style="{color: multiAnsRight(ans.value, multiSelectData[idx].Ans)? 'green':'red'}">
-        </cell>
-      </group>
-        -->
-    </div>
-    <group v-if="form.yesAns.length > 0">
-      <cell title="判断题" :value="'请选择正确或错误，每题'+yesScore+'分，共'+yesScore*form.yesAns.length+'分'"></cell>
     </group>
-    <div v-for="(ans, idx) in form.yesAns" :key="'ya' + idx">
-      <checklist :title="'问题'+ (idx + 1) +'：' + yesData[idx].Problem"
-        :label-position="'right'" required :max="1" :min="1" :options="yesData[idx].Opts" v-model="ans.value"></checklist>
-      <!--
+    <div v-show="showTypeProblem(showProblemSetting, '1')">
       <group>
-        <cell v-show="hasSubmit"
-          :title="singleAnsRight(ans.value[0], yesData[idx].RightAns[0]) ? '恭喜您答对了':'回答错误，答案应为'+(yesData[idx].RightAns[0] === 1 ? '正确': '错误')"
-          :style="{color: singleAnsRight(ans.value[0], yesData[idx].RightAns[0]) ? 'green':'red'}">
-        </cell>
+        <cell title="单选题" :value="'每个题目仅一个选项正确，每题'+selectScore+'分，共'+selectScore*form.selectAns.length+'分'"></cell>
       </group>
-      -->
+      <div v-for="(ans, idx) in form.selectAns" :key="'sa' + idx">
+        <checklist :title="'问题'+ (idx + 1) +'：' + selectData[idx].Problem" :label-position="'right'" required :max="1"
+          :min="1" :options="selectData[idx].Opts" v-model="ans.value"></checklist>
+          <!--
+        <group>
+          <cell v-show="hasSubmit"
+            :title="singleAnsRight(ans.value, selectData[idx].Ans[0]) ? '恭喜您答对了':'回答错误，答案应为'+selectData[idx].Ans[0]"
+            :style="{color: singleAnsRight(ans.value, selectData[idx].Ans[0])? 'green':'red'}">
+          </cell>
+        </group>
+          -->
+      </div>
+    </div>
+    <div v-show="showTypeProblem(showProblemSetting, '2')">
+      <group>
+        <cell title="多选题" :value="'每个题目有一个或多个选项正确，每题'+multiSelectScore+'分，共'+multiSelectScore*form.multiSelectAns.length+'分'"></cell>
+      </group>
+      <div v-for="(ans, idx) in form.multiSelectAns" :key="'msa' + idx">
+        <checklist :title="'问题'+ (idx + 1) +'：' + multiSelectData[idx].Problem" :label-position="'right'" required
+          :max="multiSelectData[idx].Opts.length" :min="1" :options="multiSelectData[idx].Opts" v-model="ans.value">
+        </checklist>
+          <!--
+        <group>
+          <cell v-show="hasSubmit"
+            :title="multiAnsRight(ans.value, multiSelectData[idx].Ans) ? '恭喜您答对了':'回答错误，答案应为'+getMultiAnsStr(multiSelectData[idx].Ans)"
+            :style="{color: multiAnsRight(ans.value, multiSelectData[idx].Ans)? 'green':'red'}">
+          </cell>
+        </group>
+          -->
+      </div>
+    </div>
+    <div v-show="showTypeProblem(showProblemSetting, '3')">
+      <group v-if="form.yesAns.length > 0">
+        <cell title="判断题" :value="'请选择正确或错误，每题'+yesScore+'分，共'+yesScore*form.yesAns.length+'分'"></cell>
+      </group>
+      <div v-for="(ans, idx) in form.yesAns" :key="'ya' + idx">
+        <checklist :title="'问题'+ (idx + 1) +'：' + yesData[idx].Problem"
+          :label-position="'right'" required :max="1" :min="1" :options="yesData[idx].Opts" v-model="ans.value"></checklist>
+        <!--
+        <group>
+          <cell v-show="hasSubmit"
+            :title="singleAnsRight(ans.value[0], yesData[idx].RightAns[0]) ? '恭喜您答对了':'回答错误，答案应为'+(yesData[idx].RightAns[0] === 1 ? '正确': '错误')"
+            :style="{color: singleAnsRight(ans.value[0], yesData[idx].RightAns[0]) ? 'green':'red'}">
+          </cell>
+        </group>
+        -->
+      </div>
     </div>
     <x-button type="primary" @click.native="submitQuestion">提交作答</x-button>
     <div v-show="hasSubmit">
@@ -55,45 +66,51 @@
       <group>
         <cell title="错题回顾"></cell>
       </group>
-      <group v-show="selErrIdx.length != 0">
-        <cell title="单选题" value=""></cell>
-      </group>
-      <div v-for="(ans, idx) in selErrIdx" :key="'sea' + idx">
-        <checklist :title="'问题'+ (ans + 1) +'：' + selectData[ans].Problem" :label-position="'right'" required :max="1"
-          :min="1" :options="selectData[ans].Opts" v-model="form.selectAns[ans].value" disabled></checklist>
+      <div v-show="showTypeProblem(showProblemSetting, '1') && selErrIdx.length != 0">
         <group>
-          <cell
-            :title="'回答错误，答案应为'+selectData[ans].Ans[0]"
-            :style="{color: 'red'}">
-          </cell>
+          <cell title="单选题" value=""></cell>
         </group>
+        <div v-for="(ans, idx) in selErrIdx" :key="'sea' + idx">
+          <checklist :title="'问题'+ (ans + 1) +'：' + selectData[ans].Problem" :label-position="'right'" required :max="1"
+            :min="1" :options="selectData[ans].Opts" v-model="form.selectAns[ans].value" disabled></checklist>
+          <group>
+            <cell
+              :title="'回答错误，答案应为'+selectData[ans].Ans[0]"
+              :style="{color: 'red'}">
+            </cell>
+          </group>
+        </div>
       </div>
-      <group v-show="multiSelErrIdx.length != 0">
-        <cell title="多选题"></cell>
-      </group>
-      <div v-for="(ans, idx) in multiSelErrIdx" :key="'msea' + idx">
-        <checklist :title="'问题'+ (ans + 1) +'：' + multiSelectData[ans].Problem" :label-position="'right'" required
-          :max="multiSelectData[ans].Opts.length" :min="1" :options="multiSelectData[ans].Opts"
-          v-model="form.multiSelectAns[ans].value" disabled></checklist>
+      <div v-show="showTypeProblem(showProblemSetting, '2') && multiSelErrIdx.length != 0">
         <group>
-          <cell 
-            :title="'回答错误，答案应为'+getMultiAnsStr(multiSelectData[ans].Ans)"
-            :style="{color: 'red'}">
-          </cell>
+          <cell title="多选题"></cell>
         </group>
+        <div v-for="(ans, idx) in multiSelErrIdx" :key="'msea' + idx">
+          <checklist :title="'问题'+ (ans + 1) +'：' + multiSelectData[ans].Problem" :label-position="'right'" required
+            :max="multiSelectData[ans].Opts.length" :min="1" :options="multiSelectData[ans].Opts"
+            v-model="form.multiSelectAns[ans].value" disabled></checklist>
+          <group>
+            <cell 
+              :title="'回答错误，答案应为'+getMultiAnsStr(multiSelectData[ans].Ans)"
+              :style="{color: 'red'}">
+            </cell>
+          </group>
+        </div>
       </div>
-      <group v-show="yesErrIdx.length != 0">
-        <cell title="判断题" value=""></cell>
-      </group>
-      <div v-for="(ans, idx) in yesErrIdx" :key="'yaa' + idx">
-        <checklist :title="'问题'+ (ans + 1) +'：' + yesData[ans].Problem" :label-position="'right'" required :max="1"
-          :min="1" :options="yesData[ans].Opts" v-model="form.yesAns[ans].value" disabled></checklist>
+      <div v-show="showTypeProblem(showProblemSetting, '3') && yesErrIdx.length != 0">
         <group>
-          <cell
-            :title="'回答错误，答案应为'+(yesData[ans].Ans[0] === '1' ? '正确': '错误')"
-            :style="{color: 'red'}">
-          </cell>
+          <cell title="判断题" value=""></cell>
         </group>
+        <div v-for="(ans, idx) in yesErrIdx" :key="'yaa' + idx">
+          <checklist :title="'问题'+ (ans + 1) +'：' + yesData[ans].Problem" :label-position="'right'" required :max="1"
+            :min="1" :options="yesData[ans].Opts" v-model="form.yesAns[ans].value" disabled></checklist>
+          <group>
+            <cell
+              :title="'回答错误，答案应为'+(yesData[ans].Ans[0] === '1' ? '正确': '错误')"
+              :style="{color: 'red'}">
+            </cell>
+          </group>
+        </div>
       </div>
     </div>
   </div>
@@ -155,7 +172,13 @@ export default {
       yesErrIdx: [],
       selectScore: selectScore,
       multiSelectScore: multiSelectScore,
-      yesScore: yesScore
+      yesScore: yesScore,
+      showProblemSetting: [],
+      showProblemOpts: [
+        {key: '1', value: '单选'},
+        {key: '2', value: '多选'},
+        {key: '3', value: '判断'}
+      ]
     }
   },
   methods: {
@@ -218,6 +241,20 @@ export default {
     },
     getMultiAnsStr (ans) {
       return ans.join('')
+    },
+    showTypeProblem (ans, pType) {
+      if (ans === null) {
+        return false
+      }
+      if (ans.length === 0) {
+        return false
+      }
+      for (let i = 0; i < ans.length; i++) {
+        if (ans[i] === pType) {
+          return true
+        }
+      }
+      return false
     }
   }
 }
